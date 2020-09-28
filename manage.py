a9f2644210117765dc1e9b5fd13f463cf4b1fbbe
mwalo254 <mwalonick@gmail.com>
2020-09-28 11:51:39 +0300

    Added missing os import block

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
-from app import create_app,db
+import os
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
-from app.models import User,Role, Pitch, Comment
-from  flask_migrate import Migrate, MigrateCommand
+
+from app import create_app, db
+from app.models import User, Role, Pitch, Comment
 
 # Creating app instance
 app = create_app(os.environ.get("ENVIRONMENT"))
